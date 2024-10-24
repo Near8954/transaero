55777c6c4c743c0132cc3c8c3c9d80d69cdaaaad
Near8954 <roma.baldin@gmail.com>
2024-10-25 00:22:09 +0300

    deleting comments added

--- main.cpp
+++ main.cpp
@@ -2,19 +2,41 @@
 
 #include <iostream>
 
-int main() {
+void removeBetweenHashes(char *str) {
+    char *readPtr = str;
+    char *writePtr = str;
+    bool insideHash = false;
 
+    while (*readPtr != '\0') {
+        if (*readPtr == '#') {
+            insideHash = !insideHash;
+        } else if (!insideHash) {
+            *writePtr++ = *readPtr;
+        }
+        ++readPtr;
+    }
+    *writePtr = '\0';
+}
+
+int main() {
     std::ifstream code("../src/code.txt");
     code.seekg(0, std::ios::end);
-    const size_t size = code.tellg();
+    size_t size = code.tellg();
     code.seekg(0, std::ios::beg);
     const auto text = new char[size];
     code.read(text, size);
-
+    removeBetweenHashes(text);
+    auto it = text;
+    size_t i = 0;
+    while (*it != '\0') {
+        ++it;
+        ++i;
+    }
+    size = i;
 
     Lexical_analyzer analyzer;
     auto b = analyzer.get_lexemes(text, size);
-    for (const auto & i : b) {
+    for (const auto &i: b) {
         std::cout << i << "\n";
     }
 }
