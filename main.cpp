0d1cda5b1a358f359dd0089e9d84dfd0a24933bd
Near8954 <roma.baldin@gmail.com>
2024-11-12 23:36:57 +0300

    minor fixes

--- main.cpp
+++ main.cpp
@@ -3,10 +3,4 @@
 
 int main() {
 
-
-    Lexical_analyzer analyzer;
-    auto b = analyzer.get_lexemes(text, size);
-    for (const auto &lexeme: b) {
-        std::cout << lexeme << "\n";
-    }
 }
