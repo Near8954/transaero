0d684dd774a2d5609ffa88ffd0f8fe9723b11610
Scesss <kiriell55@gmail.com>
2024-12-12 19:01:10 +0300

    tid added

---
+++ src/tid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Иванов Кирилл on 12.12.2024.
+//
+
+#include "tid.h"
---
+++ src/tid.h
@@ -0,0 +1,14 @@
+//
+// Created by Иванов Кирилл on 12.12.2024.
+//
+
+#ifndef TRANS_TID_H
+#define TRANS_TID_H
+
+
+class tid {
+
+};
+
+
+#endif //TRANS_TID_H
